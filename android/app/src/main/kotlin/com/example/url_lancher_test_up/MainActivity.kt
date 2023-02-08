cf5c6a77a7e6c7197d291236abea05bb82d605f9
YUUSKE <yusuke.1173ab@gmail.com>
2023-02-08 16:34:58 +0900

    first　commit

---
+++ android/app/src/main/kotlin/com/example/url_lancher_test_up/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.url_lancher_test_up
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            final url = Uri.parse('https://www.youtube.com/');
+            launchUrl(url);
+          },
+          child: const Text('Youtubeを表示'),
+        ),
+      ),
+    );
+  }
+}
